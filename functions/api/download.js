191936b060b0727207c963856a818c34db3031a9
alghtanyfahd-ops <alghtanyfahd@gmail.com>
2026-07-14 01:10:15 +0300

    Add image download functionality to API

--- functions/api/download.js
+++ functions/api/download.js
@@ -1,1 +1,18 @@
+export async function onRequest(context) {
 
+    if (context.request.method !== "POST") {
+        return new Response("Method Not Allowed", {
+            status: 405
+        });
+    }
+
+    const image = await context.request.arrayBuffer();
+
+    return new Response(image, {
+        headers: {
+            "Content-Type": "image/jpeg",
+            "Content-Disposition": 'attachment; filename="resized-image.jpg"'
+        }
+    });
+
+}
