2e159f07f055558abef9037bcca45d7469281c4d
alghtanyfahd-ops <alghtanyfahd@gmail.com>
2026-07-13 10:06:38 +0300

    Add Pi Network SDK initialization and login function
    Initialize Pi Network SDK and implement login function.

--- pi.js
+++ pi.js
@@ -1,1 +1,51 @@
+// Pi Network SDK
 
+Pi.init({
+    version: "2.0",
+    sandbox: true
+});
+
+
+window.loginPi = function(){
+
+    Pi.authenticate(
+        ["username"],
+        function(auth){
+
+            console.log(
+                "Pi user:",
+                auth.user.username
+            );
+
+
+            const box =
+            document.getElementById("piUser");
+
+
+            if(box){
+
+                box.textContent =
+                "مرحباً " +
+                auth.user.username;
+
+            }
+
+
+            localStorage.piUser =
+            auth.user.username;
+
+
+        },
+
+        function(error){
+
+            console.log(
+                "Pi Login Error",
+                error
+            );
+
+        }
+
+    );
+
+};
